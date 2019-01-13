1a7646c86623a474493e19d98a6092d9dd84809e
duch94 <vtduch@gmail.com>
2019-01-14 01:43:04 +0300

    added readme; added method for getting document from collection (not working properly yet); added implementation of ReadUserHandler

--- clients/mongodb.go
+++ clients/mongodb.go
@@ -38,3 +38,31 @@ func SendDocument(dbName string, collName string, doc map[string]interface{}) (i
 	}
 	return res.InsertedID, nil
 }
+
+// GetDocumentByLogin is function for finding users by user login
+func GetDocumentByLogin(dbName string, collName string, login string) (interface{}, error) {
+	client, err := mongo.NewClient(mongoAddress)
+	if err != nil {
+		return nil, err
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	filter := make(map[string]interface{})
+	filter["login"] = login
+	collection := client.Database(dbName).Collection(collName)
+	res := collection.FindOne(ctx, bson.M(filter))
+
+	err = client.Disconnect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var resDoc interface{}
+	res.Decode(resDoc)
+
+	return resDoc, nil
+}
--- handlers.go
+++ handlers.go
@@ -14,23 +14,45 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 
 // CreateUserHandler is handler for /createUser method
 func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		errorMessage := "CreateUser request was performed not by POST method"
+		fmt.Fprintf(w, errorMessage)
+		panic(fmt.Sprintln(errorMessage))
+	}
+
+	r.ParseForm()
+
 	newUser := make(map[string]interface{})
-	newUser["name"] = "name"
-	newUser["password"] = "password"
-	newUser["dob"] = "01-01-1970"
-	newUser["login"] = "email@mailbox.com"
+	newUser["name"] = r.PostForm.Get("name")
+	newUser["password"] = r.PostForm.Get("password")
+	newUser["dob"] = r.PostForm.Get("dob")
+	newUser["login"] = r.PostForm.Get("login")
 
 	// надо возвращать http коды
 	userID, err := clients.SendDocument("Main", "Users", newUser)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Fprintf(w, "Created user %s", userID)
+	fmt.Fprintf(w, "Created user %s\n", userID)
 }
 
 // ReadUserHandler is handler for /readUser method
 func ReadUserHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Read user")
+	if r.Method != "GET" {
+		errorMessage := "ReadUser request was performed not by GET method"
+		fmt.Fprintf(w, errorMessage)
+		panic(fmt.Sprintln(errorMessage))
+	}
+
+	url := r.URL.Query()
+	login := url.Get("login")
+
+	user, err := clients.GetDocumentByLogin("Main", "Users", login)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, user)
+	fmt.Fprintf(w, "Read user: \n%s\n", user)
 }
 
 // UpdateUserHandler is handler for /updateUser method
